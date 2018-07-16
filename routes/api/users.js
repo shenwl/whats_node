955b14e5728e91812988d520d4df3004b151b833
shenwl <shenweilun96@gmail.com>
2018-07-16 13:04:51 +0800

    fix typo

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-const User = require('../models/user')
+const User = require('../../models/user')
 
 /* GET users listing. */
 router.get('/', (req, res) => {
