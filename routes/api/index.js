6e50dfbdf0aabad576a913af0c49f02f74665b46
shenwl <shenweilun96@gmail.com>
2018-07-17 12:54:25 +0800

    使用pbkdf2算法加密

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,18 +4,38 @@ const router = express.Router()
 
 const userRouter = require('./users')
 
-const users = []
+const JWT = require('jsonwebtoken')
 
-router.get('/login', (req, res, next) => {
-  const { username } = req.query
-  req.session.user = { username }
-  console.log(req.session)
-  res.send('done')
+const User = require('../../models/user')
+
+const crypto = require('crypto')
+
+const pbkdf2Async = require('bluebird').promisify(crypto.pbkdf2)
+
+router.post('/login', (req, res, next) => {
+  (async () => {
+    const { username, password } = req.body
+    // crypto.pbkdf2：安全系数较高的加密方法，比较耗时，最好用异步
+    const cipher = await crypto.pbkdf2(password, 'dasdhui1h2hjakjd', 1000, 512, 'sha256')
+    const created = await User.insert({ username, password: cipher })
+  })()
+    .then(res => {
+    })
+    .catch(e => {
+    })
 })
 
 router.get('/hello', (req, res, next) => {
-  const { username } = req.session.user
-  res.send(`<h1>hello, ${username}</h1>`)
+  const auth = req.get('Authorization')
+  if(!auth) {
+    return res.send('no auth')
+  }
+  if(auth.indexOf('Bearer') === -1) {
+    return res.send('no auth')
+  }
+  const token = auth.split('Bearer')[1]
+  const user = JWT.verify(token, 'secretdjsadhkajdn2')
+  res.send(user)
 })
 
 router.use('/user', userRouter)
